8fd133c1504d66235d8d90aea602ebf5755311f0
muhal-git <73129339+muhal-git@users.noreply.github.com>
2024-12-10 20:59:01 +0000

    refactor: move DnsResolverApplication to a new package structure

--- src/main/java/io/github/miris/dnsresolver/DnsResolverApplication.java
+++ src/main/java/io/github/miris/dnsresolver/DnsResolverApplication.java
@@ -1,4 +1,4 @@
-package io.github.miris.dns_resolver;
+package io.github.miris.dnsresolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
