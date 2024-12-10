4059b65459fa7bcc968db87b5593bc7cf122ae5c
muhal-git <73129339+muhal-git@users.noreply.github.com>
2024-12-10 20:38:52 +0000

    first commit

---
+++ src/main/java/io/github/miris/dns_resolver/DnsResolverApplication.java
@@ -0,0 +1,13 @@
+package io.github.miris.dns_resolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnsResolverApplication.class, args);
+	}
+
+}
